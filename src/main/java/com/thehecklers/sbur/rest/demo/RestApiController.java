12d852570d947889e661b40723c987aefaf4dfcd
Sandeep Prajapati <sandeep.p4856@gmail.com>
2023-06-20 11:11:57 +0530

    chapter 4: Adding database access

--- src/main/java/com/thehecklers/sbur/rest/demo/RestApiController.java
+++ src/main/java/com/thehecklers/sbur/rest/demo/RestApiController.java
@@ -12,10 +12,13 @@ import java.util.Optional;
 @RequestMapping("/api/coffees")
 public class RestApiController {
 
-    private List<Coffee> coffees = new ArrayList<>();
+    private final ICoffeeRepository iCoffeeRepository;
+    // private List<Coffee> coffees = new ArrayList<>();
 
-    public RestApiController(){
-        coffees.addAll(List.of(
+    public RestApiController(ICoffeeRepository iCoffeeRepository){
+        this.iCoffeeRepository = iCoffeeRepository;
+
+        this.iCoffeeRepository.saveAll(List.of(
                 new Coffee("Cafe Cereza"),
                 new Coffee("Cafe Ganador"),
                 new Coffee("Cafe Lareno"),
@@ -25,27 +28,35 @@ public class RestApiController {
 
     @GetMapping
     public Iterable<Coffee> getCoffees(){
-        return coffees;
+        // return coffees;
+        return iCoffeeRepository.findAll();
     }
 
     @GetMapping("/{id}")
     public Optional<Coffee> getCoffeeById(@PathVariable String id){
+
+/*  Initial Commit: Chapter 3
         for(Coffee c: coffees){
             if(c.getId().equals(id)){
                 return Optional.of(c);
             }
         }
-        return Optional.empty();
+*/
+        return iCoffeeRepository.findById(id);
+        // return Optional.empty();
     }
 
     @PostMapping
     public Coffee postCoffee(@RequestBody Coffee coffee){
-        coffees.add(coffee);
+        // coffees.add(coffee);
+        iCoffeeRepository.save(coffee);
         return coffee;
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Coffee> putCoffee(@RequestBody Coffee coffee, @PathVariable("id") String id){
+
+/* Initial Commit: Chapter 3
         int coffeeIndex = -1;
         for(Coffee c: coffees){
             if(c.getId().equals(id)){
@@ -55,11 +66,15 @@ public class RestApiController {
         }
         return (coffeeIndex == -1) ? new ResponseEntity<>(postCoffee(coffee), HttpStatus.CREATED) :
                                      new ResponseEntity<>(coffee, HttpStatus.OK);
+*/
+        return (!iCoffeeRepository.existsById(id)) ? new ResponseEntity<>(iCoffeeRepository.save(coffee), HttpStatus.CREATED)
+                : new ResponseEntity<>(iCoffeeRepository.save(coffee), HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
     public void deleteCoffee(@PathVariable String id){
-        coffees.removeIf(c -> c.getId().equals(id));
+        // coffees.removeIf(c -> c.getId().equals(id));
+        iCoffeeRepository.deleteById(id);
     }
 
 }
