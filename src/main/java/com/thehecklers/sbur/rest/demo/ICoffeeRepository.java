bbf2ba10f9714e6f05534cb78fa5bedfb6762bde
Sandeep Prajapati <sandeep.p4856@gmail.com>
2023-06-20 18:52:45 +0530

    code refactor

--- src/main/java/com/thehecklers/sbur/rest/demo/ICoffeeRepository.java
+++
@@ -1,6 +0,0 @@
-package com.thehecklers.sbur.rest.demo;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ICoffeeRepository extends CrudRepository<Coffee, String> {
-}
