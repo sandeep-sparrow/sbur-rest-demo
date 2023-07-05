ce5c639b4388f7e44ff70f2d4316b37c9037b550
Sandeep Prajapati <sandeep.p4856@gmail.com>
2023-07-05 23:25:10 +0530

    added spring boot MVC demo sbur plane finder application

---
+++ Chapter7Java/spring-mvc/src/main/java/com/mhecklers/springmvc/PositionController.java
@@ -0,0 +1,33 @@
+package com.mhecklers.springmvc;
+
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Controller
+@RequiredArgsConstructor
+public class PositionController {
+
+    @NonNull
+    private final AircraftRepository repository;
+    private WebClient client =
+            WebClient.create("http://localhost:7634/aircraft");
+
+    @GetMapping("/aircraft")
+    public String getCurrentAircraftPositions(Model model){
+        // repository.deleteAll();
+
+        client.get()
+                .retrieve()
+                .bodyToFlux(Aircraft.class)
+                .filter(plane -> !plane.getReg().isEmpty())
+                .toStream()
+                .forEach(repository::save);
+
+        model.addAttribute("currentPositions", repository.findAll());
+        return "positions";
+    }
+}
